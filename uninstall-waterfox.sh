e843fca8febf013fcee1251c07670c9fe60fe24c
Marcelo dos Santos Mafra <msmafra@gmail.com>
2019-05-20 22:46:41 -0300

    Separated uninstall file for installations to /usr/lib64

--- uninstall-waterfox.sh
+++ uninstall-waterfox.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-# Exit on error. Append "|| true" if you expect an error.
 set -o errexit
-# Exit on error inside any functions or subshells.
 set -o errtrace
-# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
 set -o nounset
-# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 set -o pipefail
-# Turn on traces, useful while debugging but commented out by default
 #set -o xtrace
 # Unintall Waterfox from /usr/lib64
-
-# Set magic variables for current file, directory, os, etc.
 __dir="$(cd "$(dirname "${BASH_SOURCE[${__b3bp_tmp_source_idx:-0}]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[${__b3bp_tmp_source_idx:-0}]}")"
 __base="$(basename "${__file}" .sh)"
@@ -49,7 +42,7 @@ function help () {
 [[ "${__usage+x}" ]] || read -r -d '' __usage <<-'EOF' || true # exits non-zero when EOF encountered
       sudo ./uninstall-waterfox.sh
 EOF
-# shellcheck disable=SC2015
+
 [[ "${__helptext+x}" ]] || read -r -d '' __helptext <<-'EOF' || true # exits non-zero when EOF encountered
  Please run as root
 EOF
